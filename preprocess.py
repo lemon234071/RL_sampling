74ee108c34a8854edc08a5494ca25ef602586ab3
lemon <a8262589>
2019-11-14 10:07:02 +0800

    original version

---
+++ preprocess.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from onmt.bin.preprocess import main
+import torch
+import numpy as np
+import random
+
+
+def setup_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.backends.cudnn.deterministic = True
+
+
+setup_seed(2019)
+
+if __name__ == "__main__":
+    main()
