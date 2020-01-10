76adfde42619d56c5c5c11be8c53181719728c70
lemon <a8262589>
2020-01-10 22:49:07 +0800

    data gen for cls

--- trans.py
+++ trans.py
@@ -5,13 +5,13 @@ for i in range(20):
     j = (i + 1) / 10
     cmd = "cat ./data/freq0.001/pos-src-train.txt | head -n {:d} | tail -n +{:d} > ./data/t_resp/{:.1f}pos-src-train.txt".format(
         index + 50000, index, j)
-    print(cmd)
-    os.system(cmd)
+    # print(cmd)
+    # os.system(cmd)
     index += 50001
 
 for i in range(20):
     j = (i + 1) / 10
-    cmd = "CUDA_VISIBLE_DEVICES=2 python3 translate.py -gpu 0 -model ./checkpoint/freq_2gen_step_40000.pt -output ./data/t_resp/infer{:.1f}.txt -beam 1 -batch_size 2048 -src ./data/t_resp/{:.1f}src-train.txt -pos_src ./data/t_resp/{:.1f}pos-src-train.txt -max_length 30 -learned_t {.1f}".format(
+    cmd = "CUDA_VISIBLE_DEVICES=2 python3 translate.py -gpu 0 -model ./checkpoint/freq_2gen_step_40000.pt -output ./data/t_resp/infer{:.1f}.txt -beam 1 -batch_size 2048 -src ./data/t_resp/{:.1f}src-train.txt -pos_src ./data/t_resp/{:.1f}pos-src-train.txt -max_length 30 -learned_t {:.1f}".format(
         j, j, j, j)
     print(cmd)
     os.system(cmd)
