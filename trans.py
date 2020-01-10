239e3d76825f2da018a2eff0056ae3785757ca7f
lemon <a8262589>
2020-01-10 23:05:20 +0800

    data gen for cls

--- trans.py
+++ trans.py
@@ -9,10 +9,13 @@ for i in range(20):
     # os.system(cmd)
     index += 50001
 
-for i in range(20):
-    j = (i + 1) / 10
-    cmd = "CUDA_VISIBLE_DEVICES=2 python3 translate.py -gpu 0 -model ./checkpoint/freq_2gen_step_40000.pt -output ./data/t_resp/infer{:.1f}.txt -beam 1 -batch_size 2048 -src ./data/t_resp/{:.1f}src-train.txt -pos_src ./data/t_resp/{:.1f}pos-src-train.txt -max_length 30 -learned_t {:.1f}".format(
-        j, j, j, j)
-    print(cmd)
-    os.system(cmd)
-    print(i, "over")
+for file in ["valid", "test"]:
+    for i in range(20):
+        j = (i + 1) / 10
+        # cmd = "CUDA_VISIBLE_DEVICES=2 python3 translate.py -gpu 0 -model ./checkpoint/freq_2gen_step_40000.pt -output ./data/t_resp/infer{:.1f}.txt -beam 1 -batch_size 2048 -src ./data/t_resp/{:.1f}src-train.txt -pos_src ./data/t_resp/{:.1f}pos-src-train.txt -max_length 30 -learned_t {:.1f}".format(
+        #     j, j, j, j)
+        cmd = "CUDA_VISIBLE_DEVICES=2 python3 translate.py -gpu 0 -model ./checkpoint/freq_2gen_step_40000.pt -output ./data/t_resp/{}{:.1f}.txt -beam 1 -batch_size 2048 -src ./data/t_resp/src-{}.txt -pos_src ./data/freq0.001/pos-src-{}.txt -max_length 30 -learned_t {:.1f}".format(
+            file, j, file, file, j)
+        print(cmd)
+        os.system(cmd)
+        print(i, "over")
