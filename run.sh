22e062fb8356a69ffa33daa500f319b2cb183366
lemon <a8262589>
2020-02-03 22:44:41 +0800

    multi shell param update

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ elif [ "$2" = "train" ]; then
   if [ "$3" = "seq2seq" ]; then
     CUDA_VISIBLE_DEVICES="$1" python3 train.py -data "$DATA_DIR"/"$DATASET" -save_model checkpoint/"$DATA_DIR"_seq2seq -log_file ./log_dir/"$DATA_DIR"_seq2seq.log -world_size 1 -gpu_ranks 0 -global_attention mlp -word_vec_size 256 -rnn_size 512 -optim adam -param_init_glorot -rnn_type GRU -start_decay_steps 10000 -learning_rate 0.001 -train_steps 50000 -batch_size 128 -max_generator_batches 128 -report_every 500 -valid_steps 1000 -save_checkpoint_steps 5000 -statistic
   elif [ "$3" = "freq" ]; then
-    CUDA_VISIBLE_DEVICES="$1" python3 train.py -tag_gen "$4" -data "$DATA_DIR"/"$DATASET" -save_model checkpoint/"$DATA_DIR"_freq -log_file ./log_dir/"$DATA_DIR"_freq.log -world_size 1 -gpu_ranks 0 -global_attention mlp -word_vec_size 256 -rnn_size 512 -optim adam -param_init_glorot -rnn_type GRU -start_decay_steps 10000 -learning_rate 0.001 -train_steps 50000 -batch_size 128 -max_generator_batches 128 -report_every 500 -pos_vec_size 128 -valid_steps 1000 -save_checkpoint_steps 5000 -statistic
+    CUDA_VISIBLE_DEVICES="$1" python3 train.py -tag_gen "$4" -data "$DATA_DIR"/"$DATASET" -save_model checkpoint/"$DATA_DIR"_freq_"$4" -log_file ./log_dir/"$DATA_DIR"_freq_"$4".log -world_size 1 -gpu_ranks 0 -global_attention mlp -word_vec_size 256 -rnn_size 512 -optim adam -param_init_glorot -rnn_type GRU -start_decay_steps 10000 -learning_rate 0.001 -train_steps 50000 -batch_size 128 -max_generator_batches 128 -report_every 500 -pos_vec_size 128 -valid_steps 1000 -save_checkpoint_steps 5000 -statistic
     echo "Input arg 2 Is Error: $3."
   fi
 elif [ "$2" = "infer" ]; then
